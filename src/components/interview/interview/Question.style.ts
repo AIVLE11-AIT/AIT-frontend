96539a12f0fc8729debb04ea04fa30bafb2a2c33
MiJeeLee <86431761+jpg723@users.noreply.github.com>
2024-07-09 15:35:04 +0900

    [FEAT] 숫자 타이머 구현

--- src/components/interview/interview/Question.style.ts
+++ src/components/interview/interview/Question.style.ts
@@ -5,15 +5,32 @@ export const QContainer = styled.div`
     justify-content: center;
 `;
 
+// 질문 box
 export const QBox = styled.div`
     text-align: center;
     display: inline-block;
-    margin: 110px auto 30px auto;
+    margin: 70px auto 30px auto;
     padding: 20px 50px;
     font-size: 20px;
     border-radius: 30px;
     background: linear-gradient(90deg, rgba(105, 108, 234, 0.60) 0%, #696CEA 100%);
     color: white;
+`;
+
+// 타이머 제목
+export const TimerTitle = styled.div`
+    font-size: 20px;
+    border: 1px solid black;
+    width: 630px;
+    margin: 30px auto;
+    padding: 10px 0px;
+`;
+
+// 타이머
+export const Timer = styled.div`
+    margin-top: 20px;
+    font-size: 50px;
+    
 `;
 
 // Keyframes 정의: 왼쪽에서 오른쪽으로 시간이 흐르는 타이머 애니메이션
@@ -30,7 +47,7 @@ const timerAnimation = keyframes`
 export const TimerBox = styled.div`
     text-align: center;
     position: relative; /* 상대적 위치 설정 */
-    margin: 110px auto 30px auto;
+    margin: 20px auto 30px auto;
     width: 1000px;
     height: 5px;
     border-radius: 30px;
--- src/components/interview/interview/Question.tsx
+++ src/components/interview/interview/Question.tsx
@@ -24,7 +24,7 @@ function Question() {
             // 첫 번째 질문에서 3초 후에 다음 질문으로 넘어감
             timeout = setTimeout(() => {
                 setCurrentQuestionIndex(1);
-                setTimerLabel('생각 시간:');
+                setTimerLabel('준비 시간');
                 setTimeLeft(30); // 30초 타이머
                 setTimerStage('thinking');
             }, 3000);
@@ -32,7 +32,7 @@ function Question() {
             if (timerStage === 'thinking') {
                 // 생각 시간 30초 타이머
                 timeout = setTimeout(() => {
-                    setTimerLabel('답변 시간:');
+                    setTimerLabel('답변 시간');
                     setTimeLeft(60); // 60초 타이머
                     setTimerStage('answering');
                 }, 30000);
@@ -40,11 +40,13 @@ function Question() {
                 // 답변 시간 60초 타이머
                 timeout = setTimeout(() => {
                     setCurrentQuestionIndex(currentQuestionIndex + 1);
-                    setTimerLabel('생각 시간:');
+                    setTimerLabel('생각 시간');
                     setTimeLeft(30); // 다음 질문에 대한 초기 타이머 설정 (30초)
                     setTimerStage('thinking');
                 }, 60000);
             }
+        } else{ // 질문 끝나면 타이머 작동 x
+            setTimerLabel('');
         }
 
         // Countdown timer 설정
@@ -69,12 +71,13 @@ function Question() {
                 <Camera />
 
                 {/* 타이머 박스와 타이머 바 */}
-                <div>{timerLabel} {timeLeft}s</div>
+                <Q.TimerTitle>준비 시간</Q.TimerTitle>
+                <Q.Timer>{timeLeft}</Q.Timer>
                 <Q.TimerBox>
-                    {timerLabel === '생각 시간:' && (
+                    {timerLabel === '준비 시간' && (
                         <Q.Timer30Bar style={{ animationDuration: '30s' }} />
                     )}
-                    {timerLabel === '답변 시간:' && (
+                    {timerLabel === '답변 시간' && (
                         <Q.Timer60Bar style={{ animationDuration: '60s' }} />
                     )}
                 </Q.TimerBox>
