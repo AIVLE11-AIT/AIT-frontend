6d36ad4b5a476b7e369942c279c2c0ae795f67dd
MiJeeLee <86431761+jpg723@users.noreply.github.com>
2024-07-15 13:49:00 +0900

    [FEAT] 질문 리스트 제작

--- src/pages/aiResult/AiResult.style.ts
+++ src/pages/aiResult/AiResult.style.ts
@@ -1,1 +1,95 @@
 import styled from "styled-components";
+
+export const MainContainer = styled.div`
+    display: flex;
+`;
+
+// 지원자 정보 컨테이너
+export const Container = styled.div`
+    width: 290px;
+    height: 600px;
+    padding: 20px;
+    border-radius: 10px;
+    border: 1px solid #D0D2D7;
+    background: #FFF;
+    margin-left: 30px;
+`;
+
+// 지원자 사진
+export const InterviewerImage = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+// 지원자 이름, 이메일 정보 박스
+export const InterviewerInfo = styled.div`
+    text-align: center;
+    margin-top: 20px;
+`;
+
+// 이메일 정보
+export const Email = styled.div`
+    margin-top: 10px;
+    color: gray;
+`;
+
+// 목록 이동 버튼
+export const BackBtn = styled.div`
+    border: 2px solid #404146;
+    padding: 10px;
+    border-radius: 20px;
+    text-align: center;
+    margin-top: 250px;
+    cursor: pointer;
+`;
+
+// 분석 결과 컨테이너
+export const InterviewResultContainer = styled.div`
+    margin-left: 20px;
+`;
+
+export const InterviewVideo = styled.div`
+    display: flex;
+`;
+
+// 질문 리스트 박스
+export const QuestionList = styled.div`
+    width: 570px;
+    height: 300px;
+    border-radius: 10px;
+    border: 1px solid #D0D2D7;
+    background: #FFF;
+    margin-left: 10px;
+`;
+
+export const QuestionTypeBox = styled.div`
+    display: flex;
+    margin-top: 10px;
+    padding: 15px 20px;
+`;
+
+export const QuestionTypeTitle = styled.div`
+    font-size: 22px;
+    color: black;
+    font-weight: 600;
+`;
+
+// 
+export const QuestionTypeText = styled.div`
+    margin-top: 8px;
+    font-size: 12px;
+    color: gray;
+`;
+
+export const QuestionListBox = styled.div`
+    max-height: 200px; /* 스크롤을 적용할 최대 높이 설정 */
+    overflow-y: auto; /* 세로 스크롤 활성화 */
+    width: 100%;
+    margin-top: 10px;
+`;
+
+// 질문 리스트
+export const QuestionDiv = styled.div`
+    border: 1px solid black;
+    padding: 20px 30px;
+`;
--- src/pages/aiResult/AiResult.tsx
+++ src/pages/aiResult/AiResult.tsx
@@ -1,55 +1,123 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import * as A from './AiResult.style';
+
+interface InterviewerInfo {
+  name: string;
+  email: string;
+}
+
+interface Question {
+  id: number;
+  question: string;
+  // 다른 필요한 필드를 여기에 추가합니다.
+}
 
 function AiResult() {
   let { groupId, interviewerId } = useParams();
-  const [videoUrl, setVideoUrl] = useState<string | null>(null);
+  const [interviewerInfo, setInterviewerInfo] = useState<InterviewerInfo | null>(null); // 지원자 정보 저장 변수
+  const [videoUrl, setVideoUrl] = useState<string | null>(null); // 자기소개 영상 저장 변수
+  const [companyQna, setCompanyQna] = useState<Question[]>([]); // 질문 저장 변수
   const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+  const fetchVideoData = async () => {
+    try {
+      const token = sessionStorage.getItem('isLogin') || '';
+
+      const [introduceResponse, interviewerInfoResponse, companyQnaResponse] = await Promise.all([
+        axios.get(`/interviewGroup/${groupId}/interviewer/${interviewerId}/introduce/read`, {
+          headers: { Authorization: token },
+          responseType: 'blob',
+        }),
+        axios.get(`/interviewGroup/${groupId}/interviewer/${interviewerId}`, {
+          headers: { Authorization: token },
+        }),
+        axios.get(`/interviewGroup/${groupId}/companyQna/readAll`, {
+          headers: { Authorization: token },
+        }),
+      ]);
+
+      const videoBlob = new Blob([introduceResponse.data], { type: 'video/mp4' });
+      const videoUrl = URL.createObjectURL(videoBlob);
+      setVideoUrl(videoUrl); // 자기소개 영상 저장
+      setInterviewerInfo(interviewerInfoResponse.data); // 지원자 정보 저장
+      setCompanyQna(companyQnaResponse.data); // 공통질문 저장
+      
+
+    } catch (error) {
+      console.error('AxiosError:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchVideoData = async () => {
-      try {
-        const response = await axios.get(`/interviewGroup/${groupId}/interviewer/${interviewerId}/introduce/read`, {
-          headers: {
-            Authorization: sessionStorage.getItem('isLogin') || '',
-          },
-          responseType: 'blob', // 응답 데이터를 blob으로 설정
-        });
-
-        const videoBlob = new Blob([response.data], { type: 'video/mp4' });
-        const videoUrl = URL.createObjectURL(videoBlob);
-        setVideoUrl(videoUrl);
-
-      } catch (error) {
-        console.error('AxiosError:', error);
-        setError('Failed to fetch video data');
-      }
-    };
-
     fetchVideoData();
 
-    // 클린업 함수: 컴포넌트가 언마운트될 때 URL 객체를 해제
     return () => {
       if (videoUrl) {
         URL.revokeObjectURL(videoUrl);
       }
     };
-  }, [groupId, interviewerId, videoUrl]);
+  }, [groupId, interviewerId]);
+
+  function onClickBackBtn() {
+    navigate(`/interviewer-list/${groupId}`)
+  }
 
   return (
-    <div>
-      {error ? (
-        <div>{error}</div>
-      ) : videoUrl ? (
-        <video controls>
-          <source src={videoUrl} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      ) : (
-        <div>Loading...</div>
-      )}
-    </div>
+    <A.MainContainer>
+      <A.Container>
+        <A.InterviewerImage>
+          <img src={process.env.PUBLIC_URL + '/images/Image.svg'} />
+        </A.InterviewerImage>
+        {interviewerInfo ? (
+          <A.InterviewerInfo>
+            <div>{interviewerInfo.name}</div>
+            <A.Email>{interviewerInfo.email}</A.Email>
+          </A.InterviewerInfo>
+        ) : (
+          <div>Loading interviewer info...</div>
+        )}
+        <A.BackBtn onClick={onClickBackBtn}>목록으로 이동</A.BackBtn>
+      </A.Container>
+
+      <A.InterviewResultContainer>
+        <A.InterviewVideo>
+          <div>
+            {error ? (
+              <div>{error}</div>
+            ) : videoUrl ? (
+              <video controls style={{ width: '500px', height: '300px' }}>
+                <source src={videoUrl} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            ) : (
+              <div>Loading...</div>
+            )}
+          </div>
+          <A.QuestionList>
+            <A.QuestionTypeBox>
+              <div>
+                <A.QuestionTypeTitle>질문 리스트</A.QuestionTypeTitle>
+                <A.QuestionTypeText>질문을 클릭하면 해당하는 지원자의 면접 영상을 확인할 수 있습니다.</A.QuestionTypeText>
+              </div>
+            </A.QuestionTypeBox>
+
+            <A.QuestionListBox>
+              <A.QuestionDiv>자기소개</A.QuestionDiv>
+              {companyQna.length > 0 ? (
+                  companyQna.map((question) => (
+                    <A.QuestionDiv key={question.id}>{question.question}</A.QuestionDiv>
+                  ))
+                ) : (
+                  <div>Loading questions...</div>
+              )}
+            </A.QuestionListBox>
+          </A.QuestionList>
+        </A.InterviewVideo>
+      </A.InterviewResultContainer>
+    </A.MainContainer>
   );
 }
 
