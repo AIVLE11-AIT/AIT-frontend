0526f0c4153dc41d4b7879408dabc5edb4a79ae4
tiagyu <tkroh1997@naver.com>
2024-07-19 11:44:48 +0900

    저장

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -5,7 +5,7 @@ module.exports = function(app: Application) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3000',
+      target: 'https://localhost:3000//',
       changeOrigin: true,
     })
   );
