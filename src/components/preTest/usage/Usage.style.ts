64d891753e935ed5b1f785c54efdc5bdebf9cb92
MiJeeLee <86431761+jpg723@users.noreply.github.com>
2024-07-20 17:52:20 +0900

    [ADD] 면접 사전 화면 설명 추가

--- src/components/preTest/usage/Usage.style.ts
+++ src/components/preTest/usage/Usage.style.ts
@@ -4,13 +4,13 @@ import styled from "styled-components";
 export const UsageComponent = styled.div`
     position: relative;
     width: 1160px;
-    height: 496px;
     flex-shrink: 0;
     border-radius: 35px;
     border: 1px solid #D0D2D7;
     background: #FFF;
     box-shadow: 0px 4px 4px 4px rgba(0, 0, 0, 0.05);
     margin: 88px auto;
+    padding: 0px 0px 50px 0px;
 `;
 
 // 순서 아이콘
@@ -38,4 +38,24 @@ export const UsageTitle = styled.div`
     font-weight: 600;
     margin-top: 78px;
     margin-left: 70px;
+`;
+
+// 설명 메인
+export const UsageMain = styled.div`
+    display: flex;
+    margin-top: 20px;
+    margin-left: 72px;
+`;
+
+// 설명 내용
+export const UsageText = styled.div`
+    width: 500px;
+    line-height: 1.5;
+    margin-top: 15px;
+`;
+
+export const UsageImage = styled.img`
+    margin-left: 10px;
+    width: 600px;
+    height: 350px;
 `;
--- src/pages/interview/interview/InterviewExit.tsx
+++ src/pages/interview/interview/InterviewExit.tsx
@@ -26,7 +26,7 @@ function InterviewExit() {
     <div>
         <I.MainContainer>
             <I.ExitTitle>면접이 완료되었습니다.</I.ExitTitle>
-            <I.ExitText>수고하셨습니다:) 창을 종료해도 좋습니다.<br/>AIT 시스템과 관련된 문의는 dlalwl723@naver.com로 문의 바랍니다.</I.ExitText>
+            <I.ExitText>수고하셨습니다:) 창을 종료해도 좋습니다.<br/>AIT 시스템과 관련된 문의는 aitech0311@gmail.com로 문의 바랍니다.</I.ExitText>
             <I.ExitIcon>👏🏻</I.ExitIcon>
             <I.LogoContainer><img src={process.env.PUBLIC_URL + '/images/Logo.svg'}></img></I.LogoContainer>
         </I.MainContainer>
