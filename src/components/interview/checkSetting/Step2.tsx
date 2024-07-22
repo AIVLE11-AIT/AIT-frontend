3fb27cf37e4f2e937ed4573e3d7d4dd6b72ec566
MiJeeLee <86431761+jpg723@users.noreply.github.com>
2024-07-22 16:08:08 +0900

    [FIX] 지원자 약관 동의 내용 변경

--- src/components/interview/checkSetting/Step2.tsx
+++ src/components/interview/checkSetting/Step2.tsx
@@ -69,31 +69,62 @@ function Step2() {
               <S.CheckBox checked={check1} onChange={onClickCheck1} />필수 약관에 모두 동의합니다.
             </S.AgreeBox>
 
-            <S.Step2Info>응시자 서약</S.Step2Info>
+            <S.Step2Info>개인정보 수집 및 이용 동의</S.Step2Info>
             <S.AgreeContentBox>
-              <S.Content size="15px" weight="600">[면접 저작권 보호 및 유출 금지 동의]</S.Content>
               <S.Content size="14px" weight="500">
-                본 면접 질문은 저작권의 보호를 받으며, 당사는 면접 질문에 대한 정보를 복제, 공중송신,<br />
-                배포하건 2차 저작물을 작성하는 등의 행위를 금합니다. 면접에 대한 정보를 당사의 동의 없이 타인에게<br />
-                공개하거나 전달하는 행위는 당사의 재산을 침해하는 것으로, 당사의 재산을 침해하는 이는 저작권법에 따라 5년
-                이하의 징역 또는 5천만 원 이하의 벌금에 처할 수 있습니다.
+                AIT(이하 "회사"라 합니다)는 AIT 사이트 이용을 위해 필요한 최소한의 범위로 개인정보를 수집합니다.<br/>
+                회사에서 제공하는 서비스 유형에 따라 다음과 같이 개인정보를 수집, 이용, 보유 및 파기하고 있습니다.
               </S.Content>
-              <S.Content size="15px" weight="600">[부정행위 처리 동의]</S.Content>
+              <S.Content size="15px" weight="600">[항목]</S.Content>
               <S.Content size="14px" weight="500">
-                면접 진행 중 기업에서 규정한 명백한 부정행위가 확인되는 경우, 면접 응시 불가 및 실격 처리 등의 조치가<br />
-                이루어질 수 있음을 확인하였으며 이에 동의합니다.
+                이름, 생년월일, 이메일, 자기소개서, 사진, 면접영상, 답변내용
+              </S.Content>
+              <S.Content size="15px" weight="600">[수집목적]</S.Content>
+              <S.Content size="14px" weight="500">
+                AIT 서비스 이용시의 본인 확인 및 평가 기업 제공	
+              </S.Content>
+              <S.Content size="15px" weight="600">[보유 및 이용 기간]</S.Content>
+              <S.Content size="14px" weight="500">
+              1) 회사는 법령에 따른 개인정보 보유∙이용기간 또는 이용자로부터 개인정보를 수집 시에 동의 받은 개인정보 보유∙이용기간 내에서 개인정보를 처리하며, 개인정보의 처리 목적이 달성된 후에는 해당 정보를 지체 없이 파기합니다.
+              2) 개인정보 처리 및 보유 기간은 다음과 같으며, 아래 각 항목에 대하여 이용자가 명시적으로 파기 요청을 하는 경우 지체 없이 파기합니다.
+              - 면접 서비스 종료시로부터 30일 후 즉시 파기<br/> 
+              - 면접자가 개인정보에 대한 권리 행사를 통해 수집 및 이용 동의의 철회 또는 개인정보 삭제를 요청하는 경우
+              </S.Content>
+              <S.Content size="15px" weight="600">[동의를 거부할 권리가 있다는 사실과 동의 거부에 따른 불이익 내용]</S.Content>
+              <S.Content size="14px" weight="500">
+              이용자는 개인정보 수집 및 이용에 대한 동의를 거부할 권리가 있습니다. 다만, 필수항목의 수집 및 이용에 대해 동의하지 않을 경우에는 AIT 서비스를 이용하실 수 없습니다.
               </S.Content>
             </S.AgreeContentBox>
             <S.AgreeBox>
               <S.CheckBox checked={check2} onChange={onClickCheck2} />동의합니다.
             </S.AgreeBox>
 
-            <S.Step2Info>개인정보 수집 및 이용 동의</S.Step2Info>
+            <S.Step2Info>개인정보 제3자 제공 동의</S.Step2Info>
             <S.AgreeContentBox>
-              <S.Content size="15px" weight="600">[공정한 면접 진행을 위한 응시자 감독]</S.Content>
               <S.Content size="14px" weight="500">
-                공정한 면접 진행과 면접자의 AI분석을 위한 응시자 감독을 위해 컴퓨터 화면 기록, 웹캠으로 촬영된 영상을<br />
-                면접 종료 후 30일동안 보유 및 이용할 수 있습니다.
+                AIT(이하 "회사"라 합니다) 다음과 같이 개인정보를 제3자에게 제공하고 있습니다.
+              </S.Content>
+              <S.Content size="15px" weight="600">[개인정보를 제공받는 자]</S.Content>
+              <S.Content size="14px" weight="500">
+                OpenAI (ChatGPT-4o)
+              </S.Content>
+              <S.Content size="15px" weight="600">[제공하는 목적]</S.Content>
+              <S.Content size="14px" weight="500">
+                1)	자소서 기반의 면접 질문 생성
+                2)	면접 답변 평가
+              </S.Content>
+              <S.Content size="15px" weight="600">[제공하는 항목]</S.Content>
+              <S.Content size="14px" weight="500">
+                면접자의 직무, 자기소개서, 답변내용
+              </S.Content>
+              <S.Content size="15px" weight="600">[개인정보의 보유 및 이용기간]</S.Content>
+              <S.Content size="14px" weight="500">
+                OpenAI의 정책에 따라 API 입력 및 출력 데이터는 최대 30일 동안 보유되며, 그 이후에는 삭제됩니다.<br/>
+                필요에 따라 Zero Data Retention (ZDR) 정책을 적용할 수 있습니다.
+              </S.Content>
+              <S.Content size="15px" weight="600">[이용자는 회사의 개인정보 제3자 제공에 대한 동의를 거부할 권리가 있습니다.]</S.Content>
+              <S.Content size="14px" weight="500">
+                다만, 개인정보의 제3자 제공 동의를 거부할 경우 AIT 서비스를 이용하실 수 없습니다.
               </S.Content>
             </S.AgreeContentBox>
             <S.AgreeBox>
