0dc27b963876d0434547456e23260b2ff53ebb18
MiJeeLee <86431761+jpg723@users.noreply.github.com>
2024-07-22 15:10:57 +0900

    [FIX] footer 컴포넌트화

--- src/pages/home/Home.style.ts
+++ src/pages/home/Home.style.ts
@@ -275,56 +275,6 @@ export const SectionSubTitle3 = styled.h2`
   line-height: 1.7;
 `;
 
-export const Footer = styled.footer`
-  background-color: #272727;
-  padding: 10px 20px 100px 20px;
-`;
-
-export const FooterTitle = styled.h4`
-  margin: 50px auto 70px auto;
-  text-align: center;
-  font-size: 40px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-  background: linear-gradient(91deg, #C794E7 -16.62%, #696CEA 26.43%, #A077FF 68.84%, #484CF8 111.88%);
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-`;
-
-export const FooterSection = styled.div`
-  margin: 20px;
-  margin-left: 50px;
-  display: flex;
-  flex-direction: column;
-  text-align: left;
- `;
-
-export const FooterSectionsWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  justify-content: flex-start;
-  align-items: flex-start;
-  margin: 0 auto; 
-  gap: 20px;
-`;
-
-export const FooterSubTitle = styled.div`
-  color: #FFF;
-  font-size: 18px;
-  font-weight: 600;
-  margin-bottom: 20px;
-  cursor: pointer;
-`;
-
-export const FooterText = styled.p`
-  margin: 7px 0;
-  color: #FFF;
-  font-size: 13px;
-`;
-
 export const Container = styled.div`
   padding: 20px;
 `;
