9c5f570023e8ea6739ea28bc495101bee15317d1
tiagyu <tkroh1997@naver.com>
2024-07-26 09:27:24 +0900

    반응 구현

--- src/components/intro/People.style.ts
+++ src/components/intro/People.style.ts
@@ -19,7 +19,6 @@ export const PeopleBox = styled.div`
     font-size: 20px;
     font-weight: 500;
     box-shadow: 1px 4px 10px 3px #E2E2FA;
-    width: 300px;
     @media (max-width: 768px) {
         width: 100%;  /* 작은 화면에서 박스가 화면에 꽉 차게 함 */
         margin: 20px 0; /* 상하 마진 조정 */
@@ -80,20 +79,29 @@ export const Frame = styled.div`
     font-weight: 500;
     box-shadow: 1px 4px 10px 3px #E2E2FA;
     width: 800px;
+
+    @media (max-width: 768px) {
+        width: 100%;  /* 작은 화면에서 박스가 화면에 꽉 차게 함 */
+        margin: 20px 0; /* 상하 마진 조정 */
+        padding: 20px;  /* 패딩 조정 */
+    }
 `;
 
 export const Frame_image = styled.div`
     margin: 20px auto;
-    width: 530px;
-    height: 320px;
+    width: 100%;
+    height: auto;
+
+    @media (max-width: 768px) {
+        width: 100%;  /* 작은 화면에서 이미지가 박스에 맞게 조정됨 */
+        height: auto;
+    }
 `;
 
 export const Frame_image_file = styled.img`
-    margin: 20px auto;
-    width: 530px;
-    height: 320px;
-    position: absolute;
-    index: 0;
+    width: 100%;
+    height: auto;
+    object-fit: cover;  /* 이미지 비율을 유지하면서 박스에 맞게 조정 */
 `;
 
 export const Frame_text = styled.div`
@@ -107,12 +115,17 @@ export const Frame_text = styled.div`
     background-color: #696CEA;
     color: white;
     position: relative;
-    index: 1;
+    z-index: 1;
     cursor: pointer;
 
     transition: background-color 0.3s ease; /* Add transition for smooth color change */
 
     &:hover {
         background-color: #7B7DEA; /* Slightly lighter color on hover */
+    }
+    
+    @media (max-width: 768px) {
+        width: 100%;  /* 작은 화면에서 텍스트 박스가 화면에 꽉 차게 함 */
+        font-size: 14px; /* 작은 화면에서 폰트 크기 조정 */
     }
 `;
--- src/components/intro/Process.style.tsx
+++ src/components/intro/Process.style.tsx
@@ -4,10 +4,10 @@ export const ProcessContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    flex-wrap: wrap;  /* 작은 화면에서도 레이아웃이 깨지지 않도록 flex-wrap 추가 */
+    flex-wrap: wrap;
 
     @media (max-width: 768px) {
-        margin: 0 10px; /* 작은 화면에서 좌우 여백을 줌 */
+        flex-direction: column;
     }
 `;
 
@@ -23,9 +23,10 @@ export const ProcessBox = styled.div`
     width: 300px;
 
     @media (max-width: 768px) {
-        width: 100%;  /* 작은 화면에서 박스가 화면에 꽉 차게 함 */
-        margin: 20px 0; /* 상하 마진 조정 */
-        padding: 20px;  /* 패딩 조정 */
+        width: 100%;
+        padding: 20px;
+        margin: 20px 0;
+        font-size: 18px;
     }
 `;
 
@@ -36,7 +37,7 @@ export const ProcessLine = styled.div`
     border-radius: 10px;
 
     @media (max-width: 768px) {
-        width: 40px; /* 작은 화면에서 너비 조정 */
-        height: 3px; /* 작은 화면에서 높이 조정 */
+        width: 30px;
+        height: 3px;
     }
 `;
